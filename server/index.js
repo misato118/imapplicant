b24e7ec656754778c3b1eaa2b2ceac2427518e93
misato100 <misato1@my.yorku.ca>
2024-04-13 18:56:30 -0400

    Modified port

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use('/applications', applicationsRoutes);
 app.use('/research_requirements', researchRequirementsRoutes);
 app.use('/user', userRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
